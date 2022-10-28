407514e2bee657466abb5cdedf5394cecf0d7425
Wandir Pereira Filho <wandir@gmail.com>
2022-10-27 22:15:51 -0300

    Configurando JWT

--- server/constants.js
+++ server/constants.js
@@ -4,9 +4,12 @@ const DB_HOST = "web_personal";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JWT_SECRET_KEY = "minhachavesecretajsonwebtoken2022"
+
 module.exports = {
     DB_USER,
     DB_HOST,
     API_VERSION,
     IP_SERVER,
+    JWT_SECRET_KEY
 }
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const jwt = require("../utils/jwt.js");
 
 function register(req, res) {
     console.log(req.body);
@@ -34,6 +35,40 @@ function register(req, res) {
     })
 }
 
+function login(req, res){
+    const { email, password } = req.body;
+console.log(email);
+console.log(password);
+    if(!email) res.status(400).send({ msg: "O e-mail é obrigatório!"});
+    if(!password) res.status(400).send({ msg: "A senha é obrigatória!"});
+    
+    const emailLowerCase = email.toLowerCase();
+
+    User.findOne({ email: emailLowerCase}, (error, userStore) => {
+        if(error) {
+            res.status(500).send({msg: "Error interno no servidor!"});
+        } else {
+            //console.log("Password: ", password);
+            //console.log(userStore);
+            bcrypt.compare(password, userStore.password, (bcryptError, check) => {
+                if(bcryptError){
+                    res.status(500).send({msg: "Error interno no servidor!"});
+                } else if (!check){
+                    res.status(400).send({msg: "Senha incorreta!"});
+                } else if (!userStore.active) {
+                    res.status(401).send({msg: "Usuário não autorizado ou inativo!"});
+                } else {
+                    res.status(200).send({
+                        access: jwt.createAccessToken(userStore),
+                        refresh: jwt.createRefreshToken(userStore),               
+                    });
+                }
+            });
+        }
+    });
+}
+
 module.exports = {
     register,
+    login,
 };
