44aecbef5d122ed13d00d9ac3388a73d6e56cc5a
Wandir Pereira Filho <wandir@gmail.com>
2022-11-05 00:56:52 -0300

    Criando estrutura do frontend

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+export default function App() {
+  return (
+    <div>
+      
+      <h1>Em construção</h1>
+
+    <div>
+    <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+  </div>
+
+    </div>
+  )
+}
